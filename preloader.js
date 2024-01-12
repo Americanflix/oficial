6d6f2d2f60af6dcb3c8a880b6a824f1efacb7667
Americanflix <156270114+Americanflix@users.noreply.github.com>
2024-01-12 09:25:20 -0300

    Create preloader.js

---
+++ preloader.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    var div = document.getElementById('preload');
+    preload(div, 50);
+};
+function preload(el, interval){
+    var op = 1;
+    var timer = setInterval(function () {
+        if (op <= 0.0){
+            clearInterval(timer);
+            el.style.display = 'none';
+            el.className = '';
+        }
+        el.style.opacity = op;
+        op -= op * 0.0;
+    }, interval);
+}
+// quando o player cair, troque o valor 0.1 para 0.0 assim ele fica carregando infinitamente
